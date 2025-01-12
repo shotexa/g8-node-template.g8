8e30279ecd34476abf073e875919aa9dc0c6fbd1
Shota Jolbordi <shota.jolbordi@protonmail.com>
2025-01-13 01:23:41 +0400

    escape $ in jest.config.js

--- src/main/g8/jest.config.js
+++ src/main/g8/jest.config.js
@@ -2,14 +2,14 @@ module.exports = {
   testEnvironment: "node",
   moduleFileExtensions: ["js", "json", "ts"],
   moduleNameMapper: {
-    "@src/(.*)": "<rootDir>/src/$1",
-    "@test/(.*)": "<rootDir>/__tests__/$1",
+    "@src/(.*)": "<rootDir>/src/\$1",
+    "@test/(.*)": "<rootDir>/__tests__/\$1",
   },
   preset: "ts-jest/presets/default",
   transform: {
-    "^.+\\.(t|j)s$": "ts-jest",
+    "^.+\\.(t|j)s\$": "ts-jest",
   },
-  testRegex: "/__tests__/.*\\.(test|spec)\\.ts$",
+  testRegex: "/__tests__/.*\\.(test|spec)\\.ts\$",
   coverageDirectory: "coverage",
   collectCoverageFrom: ["**/*.(t|j)s"],
 }
